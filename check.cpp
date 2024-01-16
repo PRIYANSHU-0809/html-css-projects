9a59f183b9cf89cd9a3d027d24da8da0c7434246
PRIYANSHU KUMAR <92008349+PRIYANSHU-0809@users.noreply.github.com>
2024-01-16 14:10:11 +0530

    changes applied

--- check.cpp
+++ check.cpp
@@ -1,89 +1,70 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool same(string s)
-    {
-            set<char>st;
-        
-            for(auto it: s)
-            {
-                    st.insert(it);
-            }
-        
-            if(st.size()==1)return true;
-        
-        return false;
-        
-            
-    }
-    
-   int occ(string ori, string toev) {
-    
-       int wind=toev.size();
-       int c=0;
-       
-       for(int i=0; i<=ori.size()-wind; i++)
-       {
-           string temp=ori.substr(i,wind);
-           
-           if(temp==toev)c++;
-       }
-       
-       return c;
-       
-}
 int main()
 {
 
-    string s="aaaa";
+    int n;
+    cin>>n;
 
-    
-        int len=s.size();
+    vector<int>v(n);
 
-        vector<string>all;
-        
-        for(int i=0; i<len; i++)
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin>>x;
+        v[i]=x;
+    }
+
+    int q;
+    cin>>q;
+
+    vector<pair<int,int>>p;
+
+    for(int i=0; i<q; i++)
+    {
+        int x,y;
+        cin>>x>>y;
+
+        p.push_back({x,y});
+    }
+
+    vector<int>pre(n);
+
+    int s=v[0];
+    pre[0]=v[0];
+
+    for(int i=1; i<n; i++)
+    {
+        s+=v[i];
+        pre[i]=s;
+
+    }
+
+    vector<int>ans;
+    for(int i=0; i<q; i++)
+    {
+        int low=p[i].first;
+        int high=p[i].second;
+
+        int x=low-1;
+        int cal;
+
+        if(x<0)
         {
-            for(int j=1;j<=len-i; j++)
-            {
-                string temp=s.substr(i,j);
-                all.push_back(temp);
-            }
+            cal=pre[high]-0;
+
         }
-
-        vector<string>check;
-        string lamba;
-        
-        int f=0;
-        int l1=INT_MIN;
-        for(int i=0; i<all.size(); i++)
-        {
-            if(same(all[i]))
-            {
-                
-                int x=occ(s,all[i]);
-                
-                if(x>=3)
-                {
-                    
-                    check.push_back(all[i]);
-                }
-
-               
-            }
-            
+        else{
+            cal=pre[high]-pre[low-1];
         }
-        
+        ans.push_back(cal);
 
-        for(int i=0; i<check.size(); i++)
-        {
-            cout<<check[i]<<endl;
-        }
+    }
 
-        // int x=occ(s,"aa");
-
-        // cout<<x;
-
-        cout<<lamba;
+    for(int i=0; i<ans.size(); i++)
+    {
+        cout<<ans[i]<<endl;
+    }
 
 }
